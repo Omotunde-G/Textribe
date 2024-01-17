ac310b986c517af2245ad20696d2dc392828b14b
Omotunde Gabriel <elgabbito123@gmail.com>
2024-01-17 15:53:44 +0100

    Update IndexDb

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,12 +1,9 @@
 require("dotenv").config();
 const { Client } = require("pg");
 const client = new Client({
-  user: process.env.DB_USER,
-  host:  process.env.DB_HOST,
-  database:  process.env.DB_NAME,
-  password:  process.env.DB_PASSWORD,
-  port:  process.env.DB_PORT
+  connectionString: process.env.DB_CONNECTION_STRING,
 });
+
 
 client.connect();
 module.exports = client;
