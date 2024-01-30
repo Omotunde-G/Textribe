378e9a758805bcd7692877850445abf2e34f0c91
Omotunde Gabriel <elgabbito123@gmail.com>
2024-01-30 12:58:03 +0100

    added editor

--- Frontend/scripts/userdashboard.js
+++ Frontend/scripts/userdashboard.js
@@ -52,18 +52,17 @@ document.addEventListener("DOMContentLoaded", async () => {
       row.appendChild(titleCell);
 
       const storyCell = document.createElement("td");
-const contentPreview = getFirst50Words(story.content);
-storyCell.textContent = contentPreview;
-row.appendChild(storyCell);
-
-function getFirst50Words(text) {
-
-    const words = text.split(/\s+/);
-    const preview = words.slice(0, 50).join(" ");
-
-    return preview;
-}
-
+      const contentPreview = getFirst50Words(story.content);
+      storyCell.textContent = contentPreview;
+      row.appendChild(storyCell);
+      
+      function getFirst50Words(text) {
+      
+          const words = text.split(/\s+/);
+          const preview = words.slice(0, 50).join(" ");
+      
+          return preview;
+      }
       const emptyCell = document.createElement("td");
       row.appendChild(emptyCell);
 
@@ -154,3 +153,5 @@ function getFirst50Words(text) {
   // Display logged-in username on page load
   displayLoggedInUsername();
 });
+
+
