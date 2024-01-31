9c52c8cb263ab57b3f66fda8d885886418eeb300
Omotunde Gabriel <elgabbito123@gmail.com>
2024-01-31 13:35:24 +0100

    delete

--- Backend/controllers/storiesController.js
+++ Backend/controllers/storiesController.js
@@ -1,127 +1,111 @@
-const db = require("../db/index");
+const db = require("../db/index")
 
 // creating a story
 const createStory = async (req, res) => {
-  try {
-    const { title, author, content } = req.body;
-    const { user_id } = req.params;
+    try {
 
-    // Check if required fields are present
-    if (!title || !author || !content) {
-      return res
-        .status(400)
-        .json({ message: "Title, author, content are required" });
+        const {  title, author, content } = req.body;
+          const {user_id} = req.params;
+
+        // Check if required fields are present
+        if (!title || !author || !content) {
+            return res.status(400).json({ message: 'Title, author, content are required' });
+        }
+
+        // Check if user_id is present in localStorage
+
+
+   
+
+        const result = await db.query('INSERT INTO stories (title, author, content, user_id) VALUES ($1, $2, $3, $4) RETURNING *', [title, author, content, user_id]);
+
+
+
+        res.status(201).json({ message: 'Story created successfully', story: result.rows[0] });
+    } catch (error) {
+        console.error('Error creating story:', error);
+        res.status(500).json({ message: 'Error creating story' });
     }
-
-    // Check if user_id is present in localStorage
-
-    const result = await db.query(
-      "INSERT INTO stories (title, author, content, user_id) VALUES ($1, $2, $3, $4) RETURNING *",
-      [title, author, content, user_id]
-    );
-
-    res
-      .status(201)
-      .json({ message: "Story created successfully", story: result.rows[0] });
-  } catch (error) {
-    console.error("Error creating story:", error);
-    res.status(500).json({ message: "Error creating story" });
-  }
 };
 
-const fetchAllStories = async (req, res) => {
-  try {
-    const result = await db.query("SELECT * FROM stories");
-    res.status(201).json({ stories: result.rows });
-  } catch (error) {
-    console.error("Error fetching stories:", error);
-    res.status(500).json({ message: "Error Fetching Stories" });
-  }
-};
 
-const deteleStory = async (req, res) => {
-  try {
-    const { story_id } = req.params;
 
-    const result = await db.query("DELETE FROM stories WHERE story_id = $1", 
-    [story_id]);
-    if (result.rowCount === 1) {
-      res.status(200).json({ message: "Story Deleted" });
-    } else {
-      res.status(404).json({ message: "Story not found" });
+  const fetchAllStories =async (req, res) =>{
+    try {
+        const result = await db.query ('SELECT * FROM stories');
+        res.status(201).json({stories: result.rows});
+    } catch (error) {
+        console.error('Error fetching stories:', error);
+        res.status(500).json({ message: 'Error Fetching Stories'})
+        
     }
-  } catch (error) {
-    console.error("Error deleting story:", error);
-    res.status(500).json({ message: "Error deleting story" });
-  }
-};
+  }   
+
+
+
+
+const deteleStory =  async (req, res) =>{
+    try {
+        const { storyId } = req.params; 
+         const result = await db.query('DELETE FROM stories WHERE story_id = $1', [storyId])
+         if(result.rowCount === 1) {
+            res.status(200).json({message:'Story Deleted'})
+         }else {
+            res.status(404).json({ message: 'Story not found' });
+         }
+    } catch (error) {
+        console.error('Error deleting story:', error);
+        res.status(500).json({ message: 'Error deleting story' });
+    }
+}
 
 const editStoryById = async (req, res) => {
-  try {
-    const { storyId } = req.params;
-    const { title, content, author } = req.body;
+    try {
+        const { storyId } = req.params;
+        const { title, content, author } = req.body;
 
-    const result = await db.query(
-      "UPDATE stories SET title = $1, content = $2, author = $3 WHERE story_id = $4",
-      [title, content, author, storyId]
-    );
+        const result = await db.query('UPDATE stories SET title = $1, content = $2, author = $3 WHERE story_id = $4', [title, content, author, storyId]);
 
-    if (result.rowCount === 1) {
-      res.status(200).json({ message: "Story updated successfully" });
-    } else {
-      res.status(404).json({ message: "Story not found" });
+        if (result.rowCount === 1) {
+            res.status(200).json({ message: 'Story updated successfully' });
+        } else {
+            res.status(404).json({ message: 'Story not found' });
+        }
+    } catch (error) {
+        console.error('Error updating story:', error);
+        res.status(500).json({ message: 'Error updating story' });
     }
-  } catch (error) {
-    console.error("Error updating story:", error);
-    res.status(500).json({ message: "Error updating story" });
-  }
 };
+
+
 
 const fetchStoriesByAuthorId = async (req, res) => {
-  try {
-    const userId = req.params.user_id;
-    const result = await db.query(
-      "SELECT story_id, title, content, created_at FROM stories WHERE user_id = $1",
-      [userId]
-    );
+        try {
 
-    if (result.rows.length >= 1) {
-      res.status(200).json({ stories: result.rows });
-    } else {
-      res
-        .status(404)
-        .json({ message: "No stories found for the current user" });
-    }
-  } catch (error) {
-    console.error("Error fetching stories by current user:", error);
-    // Log the error more systematically, e.g., using a logging library
-    res.status(500).json({ message: "Internal Server Error" });
-  }
+            const userId = req.params.user_id;
+            console.log(userId)
+            const result = await db.query(
+                'SELECT story_id, title, content, created_at FROM stories WHERE user_id = $1',
+                [userId]
+            );
+            console.log(result)
+    
+            if (result.rows.length >= 1) {
+                res.status(200).json({ stories: result.rows });
+            } else {
+                res.status(404).json({ message: 'No stories found for the current user' });
+            }
+        } catch (error) {
+            console.error('Error fetching stories by current user:', error);
+            // Log the error more systematically, e.g., using a logging library
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
 };
 
-const getStoryById = async (req, res) => {
-  try {
-    const storyId = req.params.story_id;
-    const result = await db.query(
-      "SELECT story_id, title, content, author FROM stories WHERE story_id = $1",
-      [storyId]
-    );
-    if (result.rows.length >= 1) {
-      res.status(200).json({ stories: result.rows });
-    } else {
-      res.status(404).json({ message: "No stories found for the id" });
-    }
-  } catch (error) {
-    console.error("Error fetching stories by this id:", error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-};
 
-module.exports = {
-  createStory,
-  fetchAllStories,
-  deteleStory,
-  editStoryById,
-  fetchStoriesByAuthorId,
-  getStoryById,
-};
+
+
+
+
+
+  module.exports= {createStory, fetchAllStories, deteleStory, editStoryById, fetchStoriesByAuthorId}
