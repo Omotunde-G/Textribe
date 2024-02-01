5d4359688cc85a38e4cb86361ef3c884e023426f
Omotunde Gabriel <elgabbito123@gmail.com>
2024-02-01 08:51:00 +0100

    index

--- Backend/index.js
+++ Backend/index.js
@@ -3,10 +3,7 @@ const app = express();
 const morgan = require('morgan')
 const cors = require("cors")
 // Middleware
-app.use(cors({
-   origin: "*"
-}
-))
+app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
